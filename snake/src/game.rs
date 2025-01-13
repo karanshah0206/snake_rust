09e407965ad1e7410725e753ea26afc5ae657f36
Karan Shah <karanshah0206@outlook.com>
2025-01-13 21:59:14 +0530

    Food location error fix

--- snake/src/game.rs
+++ snake/src/game.rs
@@ -117,8 +117,8 @@ impl Game {
 
     fn add_food(&mut self) {
         let mut rng = thread_rng();
-        let mut new_x = rng.gen_range(1..=(self.width - 1));
-        let mut new_y = rng.gen_range(1..=(self.height - 1));
+        let mut new_x = rng.gen_range(1..(self.width - 1));
+        let mut new_y = rng.gen_range(1..(self.height - 1));
 
         while self.snake.overlap_tail(new_x, new_y) {
             new_x = rng.gen_range(1..(self.width - 1));
